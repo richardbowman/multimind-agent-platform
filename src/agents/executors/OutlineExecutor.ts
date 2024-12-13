c18a56bcda20ab8b6e6977ce275d417e68afd7c5
Rick Bowman <rick@bellsant.com>
2024-12-13 13:47:54 -0600

    refactor: Update import for StructuredOutputPrompt from LM Studio service

--- src/agents/executors/OutlineExecutor.ts
+++ src/agents/executors/OutlineExecutor.ts
@@ -1,5 +1,5 @@
 import { StepExecutor, StepResult } from '../stepBasedAgent';
-import { StructuredOutputPrompt } from '../../llm/lmstudioService';
+import { StructuredOutputPrompt } from "src/llm/ILLMService";
 import LMStudioService from '../../llm/lmstudioService';
 import { ModelHelpers } from 'src/llm/helpers';
 import { getGeneratedSchema } from '../../helpers/schemaUtils';
