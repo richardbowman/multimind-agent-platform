429c7228d2409c656416aae44e950ed40871fb0d
Rick Bowman <richard.bowman@gmail.com>
2024-12-16 14:06:04 -0500

    feat: Add tasks parameter to WebSocketServer initialization

--- src/main.ts
+++ src/main.ts
@@ -134,7 +134,7 @@ await solverAgent.initialize();
 
 // Initialize WebSocket server with our storage
 import { WebSocketServer } from './web/server/WebSocketServer';
-const wsServer = new WebSocketServer(storage);
+const wsServer = new WebSocketServer(storage, tasks);
 
 setupUserAgent(storage, chatBox, inputBox, artifactManager, tasks);
 
