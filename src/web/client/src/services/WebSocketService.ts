88b7980ed75528ea3970dc8cb2331308965e2db1
Rick Bowman <rick@bellsant.com>
2025-01-07 10:51:33 -0600

    refactor: Remove unused settings-related WebSocket methods

--- src/web/client/src/services/WebSocketService.ts
+++ src/web/client/src/services/WebSocketService.ts
@@ -283,18 +283,6 @@ class WebSocketService {
     this.socket.emit('get_logs', logType);
   }
 
-  getSettings(callback: (settings: any) => void) {
-    if (this.socket) {
-      this.socket.emit('getSettings', callback);
-    }
-  }
-
-  updateSettings(settings: any) {
-    if (this.socket) {
-      this.socket.emit('updateSettings', settings);
-    }
-  }
-
   onLogs(handler: (logs: { type: string, data: any }) => void) {
     this.logHandlers.push(handler);
     return () => {
