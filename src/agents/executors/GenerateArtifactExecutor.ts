6fcd60528cb3405ef9ec3b05dda6c7b21886fc6c
Rick Bowman <richard.bowman@gmail.com>
2025-01-12 17:21:23 -0500

    refactor: Update imports in GenerateArtifactExecutor.ts

--- src/agents/executors/GenerateArtifactExecutor.ts
+++ src/agents/executors/GenerateArtifactExecutor.ts
@@ -1,4 +1,4 @@
-import { StepExecutor, StepResult } from '../stepBasedAgent';
+import { ExecutorConstructorParams, StepExecutor, StepResult } from '../stepBasedAgent';
 import { RequestArtifacts } from '../../schemas/ModelResponse';
 import { ILLMService, StructuredOutputPrompt } from "src/llm/ILLMService";
 import { ModelHelpers } from 'src/llm/modelHelpers';
