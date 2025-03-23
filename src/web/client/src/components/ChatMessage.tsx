943acca94958023026c917cf40b04fb13754fa6a
Rick Bowman <richard.bowman@gmail.com>
2025-03-23 11:08:37 -0400

    refactor: Update ChatMessageProps to use Set for unread messages

--- src/web/client/src/components/ChatMessage.tsx
+++ src/web/client/src/components/ChatMessage.tsx
@@ -37,7 +37,7 @@ interface ChatMessageProps {
     onToggleExpansion: (messageId: string) => void;
     onViewThread: (messageId: string) => void;
     onViewMetadata: (message: any) => void;
-    hasUnreadReplies: boolean;
+    unreadMessages: Set<string>;
     onMessageRead: (messageId: string) => void;
 }
 
@@ -114,10 +114,11 @@ export const ChatMessage: React.FC<ChatMessageProps> = ({
     onToggleExpansion,
     onViewThread,
     onViewMetadata,
-    hasUnreadReplies,
+    unreadMessages,
     onMessageRead
 }) => {
     const isExpanded = expandedMessages.has(message.id);
+    const hasUnreadReplies = unreadMessages.has(message.id);
     const hasThread = !currentThreadId && message.replyCount||0 > 0;
     const [attachmentsExpanded, setAttachmentsExpanded] = useState(false);
     const { artifacts: allArtifacts } = useArtifacts();
